d3890d22ebbc0b256b2cf5341a20acd8c39df8b3
David Lau <vx.drizzle@gmail.com>
2013-11-19 00:25:24 +0800

    Done basic extraction for word occurrence

--- extract.ml
+++ extract.ml
@@ -3,34 +3,66 @@ open Core.Std
 type read_status = Tag | TagEnd | Content | ContentEnd | Title | TitleEnd
 
 exception IncorrectFileFormat
-exception EmptyTagFileError
+exception EmptyWordFileError
 
-let get_tag_num tag =
-	In_channel.with_file ("tags/" ^ tag ^ ".txt")
+
+let prepare_dir dir_list =
+	List.iter ~f:(fun name -> 
+		if Sys.is_directory_exn name then ()
+		else Unix.mkdir name 
+	) dir_list
+
+let get_occurrence full_path is_tag=
+	In_channel.with_file full_path
 	~f:(fun fd ->
 		match In_channel.input_line fd with
-		| None -> raise EmptyTagFileError
+		| None -> raise EmptyWordFileError
 		| Some line -> let name = String.split ~on:' ' line in
-						List.hd_exn (List.tl_exn name))
+						if is_tag then List.nth_exn name 1
+					 	else List.nth_exn name 2)
 
 let process_tag line _ =
 	let tag_list2 = String.split ~on:' ' line in
 		List.iter tag_list2 ~f:(fun tag ->
 			if String.is_empty tag then ()
 			else begin
-				if Sys.is_file_exn ("tags/" ^ tag ^ ".txt")
+				let full_path = ("tags/" ^ tag ^ ".txt") in
+				if Sys.is_file_exn full_path
 				then begin
-					let num = string_of_int ((int_of_string (get_tag_num tag)) + 1) in
-					Out_channel.with_file ("tags/" ^ tag ^ ".txt") ~append:false
+					let num = string_of_int ((int_of_string (get_occurrence full_path true)) + 1) in
+					Out_channel.with_file full_path ~append:false
 					~f:(fun fd -> Out_channel.output_string fd (tag ^ " " ^ num ^ "\n"))
 				end
 				else begin
-					Out_channel.with_file ("tags/" ^ tag ^ ".txt")
-					~f:(fun fd -> printf "%s\n" tag; Out_channel.output_string fd (tag ^ " 1\n"))
+					Out_channel.with_file full_path
+					~f:(fun fd -> Out_channel.output_string fd (tag ^ " 1\n"))
 				end
 			end 
 		);
 		tag_list2
+
+let process_word line tag_list dir =
+	let word_list = String.split ~on:' ' line in
+		List.iter word_list ~f:(fun word ->
+			if String.is_empty word then ()
+			else begin
+				prepare_dir [(dir ^ "/" ^ word)];
+				List.iter tag_list ~f:(fun tag ->
+					let full_path = (dir ^ "/" ^ word ^ "/" ^ tag ^ ".txt") in
+					if Sys.is_file_exn full_path
+					then begin
+						let num = string_of_int ((int_of_string (get_occurrence full_path false))+1) in
+						Out_channel.with_file full_path ~append:false
+						~f:(fun fd -> Out_channel.output_string fd (word ^ " " ^ tag ^ " " ^ num ^ "\n"))
+					end
+					else begin
+						Out_channel.with_file full_path
+						~f:(fun fd -> Out_channel.output_string fd (word ^ " " ^ tag ^ " 1\n"))
+					end 
+				)
+			end 
+		);
+		tag_list
 
 let rec loop_file fd status tag_list =
 	let process_line ~fd ~status ~f ~check ~tag_list =
@@ -44,28 +76,17 @@ let rec loop_file fd status tag_list =
 	| TagEnd -> process_line ~fd ~status:Content ~check:true ~tag_list 
 			 ~f:(fun _ tag_list -> tag_list)
 	| Content -> process_line ~fd ~status:ContentEnd ~check:true ~tag_list
-			 ~f:(fun _ tag_list -> tag_list)
-			 (* split the whole string into word by word, then
-				create file for each word and then insert (word,tag,occurance)
-				into content directory *)
+			 ~f:(fun line tag_list -> process_word line tag_list "content")
 	| ContentEnd -> process_line ~fd ~status:Title ~check:true ~tag_list
 			 ~f:(fun _ tag_list -> tag_list)
 	| Title -> process_line ~fd ~status:TitleEnd ~check:true ~tag_list
-			 ~f:(fun _ tag_list -> tag_list)
-			 (* split the whole string into word by word, same with content,
-				into title directory *)
+			 ~f:(fun line tag_list -> process_word line tag_list "title")
 	| TitleEnd -> process_line ~fd ~status:Tag ~check:false ~tag_list
 			 ~f:(fun _ tag_list -> tag_list)
 	
 let extract_file filename =
 	In_channel.with_file filename 
 		~f:(fun fd -> loop_file fd Tag [])
-
-let prepare_dir dir_list =
-	List.iter ~f:(fun name -> 
-		if Sys.is_directory_exn name then ()
-		else Unix.mkdir name 
-	) dir_list
 
 let () = 
 	prepare_dir ["title";"content";"tags"];
